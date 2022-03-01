01f0db546e7b6277b4631281fe54a3f95401a6b0
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-03-01 23:21:57 +0600

    add new card

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 
+const phoneDetail = document.getElementById('phone-detail');
 const searchPhone = () =>{
-    const inputValue = document.getElementById('input-value').value;
+    const inputValue = document.getElementById('input-value');
+    const inputText = inputValue.value
     // Phone name API Calling 
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+    inputValue.value = ''
+    
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
     .then(res => res.json())
     .then(data => displayPhones(data.data))
+    phoneDetail.innerHTML = ''
+    
 }
 const displayPhones = (phones) =>{
     // console.log(phones)
@@ -24,7 +30,7 @@ const displayPhones = (phones) =>{
               <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Details</button>
             </div>
           </div>
-            </div> `;
+            </div>`;
         searchResult.appendChild(div)
     })
 }
@@ -35,24 +41,32 @@ const phoneDetails = (phoneId) =>{
     .then(res => res.json())
     .then(data => displayPhone(data.data))
     
+    
 }
     // Showing The Phone Information 
 const displayPhone = (mobil) =>{
-    console.log(mobil.mainFeatures)  
-    const phoneDetail = document.getElementById('ph-detail');
+    console.log(mobil.mainFeatures.chipSet)  
+    // const phoneDetail = document.getElementById('ph-detail');
     const div = document.createElement('div')
+    phoneDetail.innerHTML = ''
     div.innerHTML=`
-        <div class="card mx-auto border-0 shadow-lg mb-3" style="width: 18rem;">
-            <img src="${mobil.image}" class="card-img-top" alt="...">
+        <div class="card mb-3 m-auto border-0 shadow-lg mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img class = "align-items-center h-75 mt-4" src="${mobil.image}" class="img-fluid  rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
             <div class="card-body">
-                <h4>${mobil.name}</h4>
-                <li>${mobil.releaseDate}</li>
-                <li>${mobil.mainFeatures.storage}</li>
-                <li>${mobil.mainFeatures.displaySize}</li>
-                <li>${mobil.mainFeatures.chipSet}</li>
-                <li>${mobil.mainFeatures.memory}</li>
+              <h5 class="card-title">${mobil.name}</h5>
+              <p class="card-text mb-1"><span class ="fw-bold">ReleaseDate : </span>${mobil.releaseDate}</p>
+                <p class="mb-1"><span class ="fw-bold">Display : </span>${mobil.mainFeatures.displaySize}</p>
+                <p class="mb-1"><span class ="fw-bold">Storage : </span>${mobil.mainFeatures.storage}</p>
+                <p class="mb-1"><span class ="fw-bold">ChipSet : </span>${mobil.mainFeatures.chipSet}</p>
+                <p class="mb-1"><span class ="fw-bold">Memory : </span>${mobil.mainFeatures.memory}</p>
             </div>
+          </div>
         </div>
-    `;
+      </div>
+        `;
     phoneDetail.appendChild(div)
 }
